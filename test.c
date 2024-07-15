39afd5216b636887cc223c35f2b57aea46d12ffb
AnarboyevTolib <tolibjonanarboyev17@gmail.com>
2024-07-15 15:39:15 +0500

    Update test.c
    test uchun

--- test.c
+++ test.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+  int son;
+  scanf("%d", &son);
+  if (son > 10)
+    printf("son %d dan katta", son);
+  else
+    printf(100);
+  return 0;
+}

ed60153db3580989272075d642eb1106b023cf9a
AnarboyevTolib <tolibjonanarboyev17@gmail.com>
2024-07-15 15:42:42 +0500

    Create test2.c

---
+++ test2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int son;
+  scanf("%d", &son);
+  if (son > 10)
+    printf("son %d dan katta", 10);
+  else
+    printf("%d", son);
+  return 0;
+}//bu boshqa kod
